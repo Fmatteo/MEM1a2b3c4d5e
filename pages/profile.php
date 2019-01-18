a8fbbf3c269c1c89293b04e6b8b5b36891e63c49
draebard <draebard@gmail.com>
2019-01-19 00:28:01 +0800

    Changes in profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -313,7 +313,64 @@ $id = $_SESSION['id'];
 						</div>
  
             </div><!-- /.col -->
-			
+			       
+
+<!-- EDIT MODAL START ------------------------------------------------------------------------------------------------->
+       <div id = "update<?php echo $id;?>" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
+           <div class="modal-dialog modal-md">
+                      <div class="modal-content">
+
+                        <div class="modal-header box-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
+                          </button>
+                          <h4 class="modal-title" id="myModalLabel2" style="color:white;">Edit User Details</h4>
+                        </div>
+                        <div class="modal-body">
+                         <form method = "POST" action = "update_user.php"> 
+               <input type="hidden" name="user_id" value="<?php echo $id;?>">
+                <label>Full name</label>
+                  <input type="text" name = "name" class="form-control" value = "<?php echo $row['name'];?>">
+                  <br/>               
+                <label>Username</label>
+                  <input type="text" name = "username" class="form-control" value = "<?php echo $row['username'];?>">
+                  <br/>
+                <label>Password</label>
+                  <input type="password" name = "password" class="form-control" placeholder="Enter to Change Password">
+                  <br/>
+                <label>Status</label>
+                <select name = "status" class = "form-control">
+                  <option value = "active">Active</option>
+                  <option value = "inactive">Inactive</option>            
+                </select>
+                  <br/>
+                <label>Branch Name</label>
+                  <select name = "branch_id" class = "form-control">
+                    <option value = "<?php echo $row['branch_id'];?>"><?php echo $row['branch_name'];?></option>
+                    <option></option>
+                    <?php 
+                      include 'dbcon.php';                
+                    $query4=mysqli_query($con,"select * from branch")or die(mysqli_error($con));
+                    while ($row1=mysqli_fetch_array($query4)){
+                      $id3=$row1['branch_id'];
+                    ?>
+                    <option value = "<?php echo $row1['branch_id'];?>"><?php echo $row1['branch_name'];?></option>
+                    <?php } ?>
+                    
+                  </select>
+                  <br/>
+              <div style="text-align: center;">
+                <button  name = "update" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              </div>
+            </form>
+            </div>
+                        <div class="modal-footer">
+                          
+                        </div>
+                      </div>
+                    </div>
+        </div>
+<!--EDIT MODAL END ---------------------------------------------------------------------------------------------------->
 			
 	  
             
